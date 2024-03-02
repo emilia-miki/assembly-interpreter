b96a0e9e2fb25533df35fd734cef54c378d292d1
Emilia Miki <emiliamiki@icloud.com>
2024-03-02 03:45:10 +0200

    Fix bugs, make it functional
    * The program works and passes the tests on codewars

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,24 @@
 module Main where
 
--- import qualified Data.Text as T
 -- I will port to Data.Text after I get this program working
+-- import Data.Text qualified as T
 
-import Data.Char (isSpace)
+import Data.Char (isDigit, isSpace)
+import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Map.Strict qualified as M
 import Data.Maybe (mapMaybe)
 import GHC.Base (maxInt)
-import Text.Read (readMaybe)
 
 main :: IO ()
 main = interact (show . interpret)
 
 interpret :: String -> Maybe String
-interpret = output . execProgram . uncurry initProcessor . parseProgram
+interpret p = if ended endP then output endP else Nothing
+  where
+    endP = (execProgram . uncurry initProcessor . parseProgram) p
 
-data Status = StatusUndefined | StatusLess | StatusEqual | StatusGreater deriving (Eq)
+data Status = StatusUndefined | StatusLess | StatusEqual | StatusGreater deriving (Show, Eq)
 
 type Registers = M.Map String Int
 
@@ -27,6 +29,12 @@ type Labels = M.Map Label Int
 data Var where
   Lit :: Int -> Var
   Reg :: String -> Var
+  deriving (Show, Eq)
+
+data CmdArg where
+  CmdArgInt :: !Int -> CmdArg
+  CmdArgString :: !String -> CmdArg
+  CmdArgRegOrLabel :: !String -> CmdArg
   deriving (Show, Eq)
 
 data Cmd where
@@ -47,7 +55,7 @@ data Cmd where
   Jl :: Label -> Cmd
   Call :: Label -> Cmd
   Ret :: Cmd
-  Msg :: [String] -> Cmd
+  Msg :: [CmdArg] -> Cmd
   End :: Cmd
   Illegal :: String -> Cmd
   deriving (Show, Eq)
@@ -62,6 +70,7 @@ data Processor = Processor
     output :: !(Maybe String),
     ended :: !Bool
   }
+  deriving (Show)
 
 initProcessor :: [Cmd] -> Labels -> Processor
 initProcessor commands labels =
@@ -81,27 +90,30 @@ execProgram p =
   let command = M.lookup (programCounter p) (commands p)
    in case command of
         Nothing -> p
-        Just cmd -> case cmd of
-          Mov a b -> cmdMov a b p
-          Inc a -> cmdInc a p
-          Dec a -> cmdDec a p
-          Add a b -> cmdAdd a b p
-          Sub a b -> cmdSub a b p
-          Mul a b -> cmdMul a b p
-          Div a b -> cmdDiv a b p
-          Cmp a b -> cmdCmp a b p
-          Jmp l -> cmdJmp l p
-          Jne l -> cmdJne l p
-          Je l -> cmdJe l p
-          Jge l -> cmdJge l p
-          Jg l -> cmdJg l p
-          Jle l -> cmdJle l p
-          Jl l -> cmdJl l p
-          Call l -> cmdCall l p
-          Ret -> cmdRet p
-          Msg args -> cmdMsg args p
-          End -> cmdEnd p
-          Illegal str -> cmdIllegal str p
+        Just cmd -> execProgram (execCommand p cmd)
+
+execCommand :: Processor -> Cmd -> Processor
+execCommand p cmd = case cmd of
+  Mov a b -> cmdMov a b p
+  Inc a -> cmdInc a p
+  Dec a -> cmdDec a p
+  Add a b -> cmdAdd a b p
+  Sub a b -> cmdSub a b p
+  Mul a b -> cmdMul a b p
+  Div a b -> cmdDiv a b p
+  Cmp a b -> cmdCmp a b p
+  Jmp l -> cmdJmp l p
+  Jne l -> cmdJne l p
+  Je l -> cmdJe l p
+  Jge l -> cmdJge l p
+  Jg l -> cmdJg l p
+  Jle l -> cmdJle l p
+  Jl l -> cmdJl l p
+  Call l -> cmdCall l p
+  Ret -> cmdRet p
+  Msg args -> cmdMsg args p
+  End -> cmdEnd p
+  Illegal str -> cmdIllegal str p
 
 cmdMov :: Var -> Var -> Processor -> Processor
 cmdMov (Lit _) _ p = cmdIllegal "mov: first argument is not a register" p
@@ -195,32 +207,29 @@ cmdJl l p
 cmdCall :: Label -> Processor -> Processor
 cmdCall l p
   | M.notMember l $ labels p = cmdIllegal "call: no such label" p
-  | otherwise = cmdJmp l p {stack = (programCounter p + 1) : stack p}
+  | otherwise = cmdJmp l (p {stack = (programCounter p + 1) : stack p})
 
 cmdRet :: Processor -> Processor
 cmdRet p = case stack p of
   [] -> cmdIllegal "ret: not inside a function" p
   (h : t) -> p {programCounter = h, stack = t}
 
-cmdMsg :: [String] -> Processor -> Processor
+cmdMsg :: [CmdArg] -> Processor -> Processor
 cmdMsg args p = case errors of
-  [] -> p {output = Just (unwords strs)}
+  errs | null errs || all null errs -> p {programCounter = programCounter p + 1, output = Just (concat strs)}
   (err : _) -> cmdIllegal err p
+  [] -> p {programCounter = programCounter p + 1, output = Just ""}
   where
-    processedArgs = map (processMsgArg p) args
+    processedArgs = map (processCmdMsgArg p) args
     strs = map fst processedArgs
     errors = map snd processedArgs
 
-processMsgArg :: Processor -> String -> (String, String)
-processMsgArg p str
-  | '\'' `elem` str = case str of
-      ('\'' : rest) -> case reverse rest of
-        ('\'' : rst) | '\'' `notElem` rst -> (reverse rst, "")
-        _invalidString -> ("", "msg: invalid string argument")
-      _invalidString -> ("", "msg: invalid string argument")
-  | otherwise = case M.lookup str (registers p) of
-      Just value -> (show value, "")
-      Nothing -> ("", "msg: invalid argument: register not initialized")
+processCmdMsgArg :: Processor -> CmdArg -> (String, String)
+processCmdMsgArg _ (CmdArgInt num) = (show num, "")
+processCmdMsgArg _ (CmdArgString arg) = (arg, "")
+processCmdMsgArg p (CmdArgRegOrLabel reg) = case M.lookup reg (registers p) of
+  Just num -> (show num, "")
+  Nothing -> ("", "msg: register not initialized")
 
 cmdEnd :: Processor -> Processor
 cmdEnd p = p {programCounter = maxInt, ended = True}
@@ -228,21 +237,13 @@ cmdEnd p = p {programCounter = maxInt, ended = True}
 cmdIllegal :: String -> Processor -> Processor
 cmdIllegal str p = p {programCounter = maxInt, output = Just str}
 
-cleanup :: String -> [String]
-cleanup = filter (not . null) . map (unwords . words . takeWhile (/= ';')) . lines
-
 parseProgram :: String -> ([Cmd], Labels)
 parseProgram input =
   let lst = zipWith (curry parseCmdAndLabel) [0 ..] (combineAdjacent $ cleanup input)
    in (map fst lst, M.fromList $ mapMaybe snd lst)
 
-containsLabel :: String -> Bool
-containsLabel = not . any isDelimiter . takeWhile (/= ':')
-  where
-    isDelimiter c = isSpace c || c == ','
-
-containsOnlyLabel :: String -> Bool
-containsOnlyLabel str = containsLabel str && null (dropWhile (/= ':') str)
+cleanup :: String -> [String]
+cleanup = filter (not . null) . map (reverse . dropWhile isSpace . reverse . dropWhile isSpace . takeWhile (/= ';')) . lines
 
 combineAdjacent :: [String] -> [String]
 combineAdjacent [] = []
@@ -251,49 +252,184 @@ combineAdjacent (x : y : xs)
   | containsOnlyLabel x = (x ++ y) : combineAdjacent xs
   | otherwise = x : combineAdjacent (y : xs)
 
+containsOnlyLabel :: String -> Bool
+containsOnlyLabel str = containsLabel str && dropWhile (/= ':') str == ":"
+
+containsLabel :: String -> Bool
+containsLabel str = ':' `elem` str && not (any isDelimiter $ takeWhile (/= ':') str)
+  where
+    isDelimiter c = isSpace c || c == ','
+
 parseCmdAndLabel :: (Int, String) -> (Cmd, Maybe (Label, Int))
 parseCmdAndLabel (i, line)
   | containsLabel line = case splitOn ":" line of
-      (before : after) -> (parseCmd $ dropWhile isSpace $ concat after, Just (before, i))
+      (before : after) -> (parseCmd $ intercalate ":" after, Just (before, i))
       _noLabel -> (parseCmd line, Nothing)
   | otherwise = (parseCmd line, Nothing)
 
 parseCmd :: String -> Cmd
-parseCmd str = parseCmd' $ map (unwords . words) $ splitOn "," str
+parseCmd str = case maybeArgs of
+  Nothing -> Illegal $ "parse error: invalid arguments: " ++ str
+  Just args -> parseCmd' cmd args
+  where
+    preTrimmed = dropWhile isSpace str
+    cmd = takeWhile (not . isSpace) preTrimmed
+    maybeArgs = parseCmdArgs $ dropWhile isSpace $ dropWhile (not . isSpace) preTrimmed
 
-parseCmd' :: [String] -> Cmd
-parseCmd' [] = Illegal "internal error"
-parseCmd' (cmd : args) = case args of
+parseCmd' :: String -> [CmdArg] -> Cmd
+parseCmd' cmd args = case args of
   [] -> case cmd of
     "ret" -> Ret
     "end" -> End
-    _ -> Illegal cmd
-  [a] -> case cmd of
-    "inc" -> Inc (Reg a)
-    "dec" -> Dec (Reg a)
-    "jmp" -> Jmp a
-    "jne" -> Jne a
-    "je" -> Je a
-    "jge" -> Jge a
-    "jg" -> Jg a
-    "jle" -> Jle a
-    "jl" -> Jl a
-    "call" -> Call a
-    _ -> Illegal $ unwords $ cmd : args
-  [a, b] -> case cmd of
-    "mov" -> Mov (pv a) (pv b)
-    "add" -> Add (pv a) (pv b)
-    "sub" -> Sub (pv a) (pv b)
-    "mul" -> Mul (pv a) (pv b)
-    "div" -> Div (pv a) (pv b)
-    "cmp" -> Cmp (pv a) (pv b)
-    _ -> Illegal $ unwords $ cmd : args
-  arr -> case cmd of
-    "msg" -> Msg arr
-    _ -> Illegal $ unwords $ cmd : args
+    _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+  [a] -> case a of
+    CmdArgRegOrLabel rl -> case cmd of
+      "inc" -> Inc (Reg rl)
+      "dec" -> Dec (Reg rl)
+      "jmp" -> Jmp rl
+      "jne" -> Jne rl
+      "je" -> Je rl
+      "jge" -> Jge rl
+      "jg" -> Jg rl
+      "jle" -> Jle rl
+      "jl" -> Jl rl
+      "call" -> Call rl
+      "msg" -> Msg args
+      _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+    CmdArgInt _ -> case cmd of
+      "msg" -> Msg args
+      _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+    CmdArgString _ -> case cmd of
+      "msg" -> Msg args
+      _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+  [a, b] -> case a of
+    CmdArgRegOrLabel rla -> case b of
+      CmdArgRegOrLabel rlb -> case cmd of
+        "mov" -> Mov (Reg rla) (Reg rlb)
+        "add" -> Add (Reg rla) (Reg rlb)
+        "sub" -> Sub (Reg rla) (Reg rlb)
+        "mul" -> Mul (Reg rla) (Reg rlb)
+        "div" -> Div (Reg rla) (Reg rlb)
+        "cmp" -> Cmp (Reg rla) (Reg rlb)
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+      CmdArgInt num -> case cmd of
+        "mov" -> Mov (Reg rla) (Lit num)
+        "add" -> Add (Reg rla) (Lit num)
+        "sub" -> Sub (Reg rla) (Lit num)
+        "mul" -> Mul (Reg rla) (Lit num)
+        "div" -> Div (Reg rla) (Lit num)
+        "cmp" -> Cmp (Reg rla) (Lit num)
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+      CmdArgString _ -> case cmd of
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+    CmdArgInt numa -> case b of
+      CmdArgRegOrLabel rl -> case cmd of
+        "cmp" -> Cmp (Lit numa) (Reg rl)
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+      CmdArgInt num -> case cmd of
+        "cmp" -> Cmp (Lit numa) (Lit num)
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+      CmdArgString _ -> case cmd of
+        "msg" -> Msg args
+        _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+    CmdArgString _ -> case cmd of
+      "msg" -> Msg args
+      _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+  args' -> case cmd of
+    "msg" -> Msg args'
+    _ -> Illegal $ "parse error on command " ++ cmd ++ " with args " ++ show args
+
+data ParserState = ParsingWhitespace | ParsingRegName | ParsingString | ParsingInt deriving (Show)
+
+data CmdArgParser = CmdArgParser
+  { string :: !String,
+    state :: !ParserState,
+    argBuffer :: !String,
+    args :: ![CmdArg],
+    valid :: !Bool
+  }
+  deriving (Show)
+
+parseCmdArgs :: String -> Maybe [CmdArg]
+parseCmdArgs str = if valid p then Just (args p) else Nothing
   where
-    pv name =
-      let num = (readMaybe name :: Maybe Int)
-       in case num of
-            Just n -> Lit n
-            Nothing -> Reg name
+    p = newP {args = reverse $ args newP}
+    newP = parseCmdArgs' initParser
+    initParser =
+      CmdArgParser
+        { string = str,
+          state = ParsingWhitespace,
+          argBuffer = "",
+          args = [],
+          valid = False
+        }
+
+parseCmdArgs' :: CmdArgParser -> CmdArgParser
+parseCmdArgs' p | valid p = p
+parseCmdArgs' p = case state p of
+  ParsingWhitespace -> case string p of
+    [] -> p {valid = True}
+    (x : rest) -> case x of
+      sym | isDigit sym -> parseCmdArgs' $ p {state = ParsingInt}
+      sym | isSpace sym || sym == ',' -> parseCmdArgs' $ p {string = rest}
+      '\'' -> parseCmdArgs' $ p {string = rest, state = ParsingString}
+      _startOfRegisterName -> parseCmdArgs' $ p {state = ParsingRegName}
+  ParsingRegName -> case string p of
+    [] ->
+      p
+        { args = CmdArgRegOrLabel (reverse $ argBuffer p) : args p,
+          argBuffer = "",
+          valid = True
+        }
+    (x : rest) -> case x of
+      sym
+        | isSpace sym || sym == ',' ->
+            parseCmdArgs' $
+              p
+                { string = rest,
+                  state = ParsingWhitespace,
+                  args = CmdArgRegOrLabel (reverse (argBuffer p)) : args p,
+                  argBuffer = ""
+                }
+      _notWhitespace -> parseCmdArgs' $ p {string = rest, argBuffer = x : argBuffer p}
+  ParsingString -> case string p of
+    [] -> p {valid = False}
+    (x : rest) -> case x of
+      '\'' ->
+        parseCmdArgs' $
+          p
+            { string = rest,
+              state = ParsingWhitespace,
+              args = CmdArgString (reverse (argBuffer p)) : args p,
+              argBuffer = ""
+            }
+      _anythingElse -> parseCmdArgs' $ p {string = rest, argBuffer = x : argBuffer p}
+  ParsingInt -> case string p of
+    [] ->
+      p
+        { args = CmdArgInt ((read $ reverse $ argBuffer p) :: Int) : args p,
+          argBuffer = "",
+          valid = True
+        }
+    (x : rest) -> parseCmdArgs' $ case x of
+      sym
+        | sym == ',' || isSpace sym ->
+            p
+              { string = rest,
+                state = ParsingWhitespace,
+                args = CmdArgInt ((read $ reverse (argBuffer p)) :: Int) : args p,
+                argBuffer = ""
+              }
+      sym
+        | isDigit sym ->
+            parseCmdArgs' $
+              p
+                { string = rest,
+                  argBuffer = x : argBuffer p
+                }
+      _invalidCharacter -> p {valid = False}
